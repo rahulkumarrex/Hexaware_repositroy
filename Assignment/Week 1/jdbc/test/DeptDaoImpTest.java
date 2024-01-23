e57f199858a80ad105097bedc76922af7275b3af
Rahul Kumar <rk3284499@gmail.com>
2024-01-23 09:24:21 +0530

    Uploaded

---
+++ Assignment/Week 1/jdbc/test/DeptDaoImpTest.java
@@ -0,0 +1,30 @@
+package test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import jdbc.dao.IDeptDao;
+
+class DeptDaoImpTest {
+	
+
+	static IDeptDao dao;
+	
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		
+	}
+	@Test
+	void testSelectAll() {
+		
+	}
+
+}
